7d0994238d9a75dd83fdb90049426bfce6629062
jhanvi857 <jhanvip8507@gmail.com>
2026-03-27 22:49:58 +0530

    add:initial setup for backend

---
+++ server/App.java
@@ -0,0 +1,13 @@
+import com.jhanvi857.nioflow.NioFlowApp;
+
+import controller.ApiController;
+// import com.jhanvi857.nioflow.protocol.HttpStatus;
+public class App {
+    public static void main (String args[]) {
+        NioFlowApp app = new NioFlowApp();
+        app.get("/",ctx-> ctx.send("Nioflow is running !"));
+        app.get("/timetable", ApiController::getTimetable);
+        app.get("/conflicts", ApiController::getConflicts);
+        app.listen(8080);
+    }
+}
